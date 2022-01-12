1dc24a5dffc7c82f3d281192a1aa54d9fab44516
lindwurm <lindwurm.q@gmail.com>
2022-01-13 02:32:34 +0900

    soong: Change to FLOKO_VERSION
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -139,7 +139,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"CR_VERSION",
+	"FLOKO_VERSION",
 	"TARGET_DEVICE",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_TYPE",
